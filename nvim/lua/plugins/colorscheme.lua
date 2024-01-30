d423cad884d3a21214b3483ef3012bbf62a59de4
Raul Rusu <raulrusu46@gmail.com>
2024-01-30 11:57:56 +0200

    updatE

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  -- add theme
+  { "ellisonleao/gruvbox.nvim" },
+
+  -- Configure LazyVim to load theme
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
+}
